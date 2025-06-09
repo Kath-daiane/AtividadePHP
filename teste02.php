f830c0b30fc2cc5fc72ef904fe3a5690417e33b4
Kath-daiane <kath.daiane.p@gmail.com>
2025-06-09 11:09:23 -0300

    adiciona arquivo teste02.php com exemplo de variáveis em PHP

---
+++ teste02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis em PHP</title>
+</head>
+<body>
+    <h1>Variáveis em PHP</h1>
+    <?php
+         $valor = "Kath";
+
+         echo $valor . "<br>";
+
+            $valor = 9;
+            $valor = $valor + 1;
+            $valor += 1;
+
+            echo $valor;
+    ?>
+
+</body>
+</html>

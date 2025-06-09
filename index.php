46f9e828998bc7770df2154aa69166a9dd88c35b
Kath-daiane <kath.daiane.p@gmail.com>
2025-06-09 10:24:55 -0300

    adiciona arquivo index.php com exemplos de comentários e saída de dados

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// Comentatario de linha com barra dupla
+# Comentário de linha com cerquilha
+
+/* Comentário de bloco
+   com várias linhas
+*/
+// Declaração de variáveis
+$nome = "Kath"; 
+
+// Concatenação e saída de dados
+echo "Olá, " . $nome . "!<br>";
+
+echo "Olá Mundo $nome!<br>"; // saida com echo
+print ("Olá!"); // saida com print
+
+?>
